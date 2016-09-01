17ddf4bf8b0b8b0653fcdfc93014f356a4039d32
Alejandro Bonilla <abonilla@suse.com>
2016-08-31 21:59:04 -0400

    added options, output

--- job-sumaBackup.sh
+++ job-sumaBackup.sh
@@ -1,7 +1,14 @@
 #!/bin/bash
+echo -e "\e[31mEnabling network and Storage\e[0m"
+ifconfig eth0:0 192.168.124.253/24 up
 rbd -p backup map sumaBackup
+sleep 1
 mount /dev/rbd/backup/sumaBackup /mnt/
-sh /mnt/sumaBackup.sh
+echo -e "\e[32mRunning Backup Job\e[0m"
+sh /root/sumaBackup.sh
 sync
+sleep 1
+echo -e "\e[31mBackup Finished - Disabling Network and Storage\e[0m"
 umount /mnt
 rbd unmap /dev/rbd0
+ifconfig eth0:0 down
