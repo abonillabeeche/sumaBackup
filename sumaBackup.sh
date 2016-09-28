90ad30671e95deb0bea84ea9f788ff958aaf2f54
Alejandro Bonilla <abonilla@suse.com>
2016-09-28 19:38:27 -0400

    Updated colors

--- sumaBackup.sh
+++ sumaBackup.sh
@@ -20,7 +20,7 @@ do
         echo "Copying "$f
         cp -r $f /mnt/files
 done
-	echo -e "\e[32mPerforming Database Backup\e[0m"        
+	echo -e "\e[31mPerforming Database Backup\e[0m"        
         smdba backup-hot --enable=on --backup-dir=/mnt/db
-        echo -e "\e[32mSyncing Repo content\e[0m"
+        echo -e "\e[31mSyncing Repo content\e[0m"
         rsync -a /var/spacewalk/ /mnt/spacewalk/
