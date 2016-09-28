dc45cbbd038a91313e9aeed38a49f8c75bfc9642
Alejandro Bonilla <abonilla@suse.com>
2016-09-28 19:25:11 -0400

    Updated if statement

--- job-sumaBackup.sh
+++ job-sumaBackup.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-echo -e "\e[31mEnabling network and Storage\e[0m"
+
 ifconfig eth0:0 192.168.124.253/24 up
 rbd -p backup map sumaBackup
 sleep 1
@@ -11,11 +11,12 @@ then
     sh /root/sumaBackup.sh
     sync
     sleep 1
-    echo -e "\e[31mBackup Finished - Disabling Network and Storage\e[0m"
-    umount /mnt
-    rbd unmap /dev/rbd1
-    #ifconfig eth0:0 down
+    echo -e "\e[31mBackup Finished\e[0m"
 else
     echo "Error: Directory /mnt/db does not exists."
     exit 1
 fi
+echo -e "\e[31mDisabling network and Storage\e[0m"
+umount /mnt
+rbd unmap /dev/rbd1
+ifconfig eth0:0 down
